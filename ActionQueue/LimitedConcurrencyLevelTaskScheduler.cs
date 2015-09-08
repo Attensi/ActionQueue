31e920ec93949974b95d0e53190d349cbdb70988
Gard Sandholt <gard.sandholt@gmail.com>
2015-09-08 14:27:11 +0200

    Moved LimitedConcurrencyLevelTaskScheduler into other file

---
+++ ActionQueue/LimitedConcurrencyLevelTaskScheduler.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ActionQueueSpace
+{
+    public class LimitedConcurrencyLevelTaskScheduler : TaskScheduler
+    {
+        [ThreadStatic]
+        private static bool _currentThreadIsProcessingItems;
+        private readonly LinkedList<Task> _tasks = new LinkedList<Task>();
+        private readonly int _maxDegreeOfParallelism;
+        private int _delegatesQueuedOrRunning = 0;
+
+        public LimitedConcurrencyLevelTaskScheduler(int maxDegreeOfParallelism)
+        {
+            if (maxDegreeOfParallelism < 1) throw new ArgumentOutOfRangeException("maxDegreeOfParallelism");
+            _maxDegreeOfParallelism = maxDegreeOfParallelism;
+        }
+
+        protected sealed override void QueueTask(Task task)
+        {
+            lock (_tasks)
+            {
+                _tasks.AddLast(task);
+                if (_delegatesQueuedOrRunning < _maxDegreeOfParallelism)
+                {
+                    ++_delegatesQueuedOrRunning;
+                    NotifyThreadPoolOfPendingWork();
+                }
+            }
+        }
+
+        private void NotifyThreadPoolOfPendingWork()
+        {
+            ThreadPool.UnsafeQueueUserWorkItem(_ =>
+            {
+                _currentThreadIsProcessingItems = true;
+                try
+                {
+                    while (true)
+                    {
+                        Task item;
+                        lock (_tasks)
+                        {
+                            if (_tasks.Count == 0)
+                            {
+                                --_delegatesQueuedOrRunning;
+                                break;
+                            }
+
+                            item = _tasks.First.Value;
+                            _tasks.RemoveFirst();
+                        }
+
+                        base.TryExecuteTask(item);
+                    }
+                }
+                finally { _currentThreadIsProcessingItems = false; }
+            }, null);
+        }
+
+        protected sealed override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            if (!_currentThreadIsProcessingItems) return false;
+
+            if (taskWasPreviouslyQueued)
+                if (TryDequeue(task))
+                    return base.TryExecuteTask(task);
+                else
+                    return false;
+            else
+                return base.TryExecuteTask(task);
+        }
+
+        protected sealed override bool TryDequeue(Task task)
+        {
+            lock (_tasks) return _tasks.Remove(task);
+        }
+
+        public sealed override int MaximumConcurrencyLevel { get { return _maxDegreeOfParallelism; } }
+
+        protected sealed override IEnumerable<Task> GetScheduledTasks()
+        {
+            bool lockTaken = false;
+            try
+            {
+                Monitor.TryEnter(_tasks, ref lockTaken);
+                if (lockTaken) return _tasks;
+                else throw new NotSupportedException();
+            }
+            finally
+            {
+                if (lockTaken) Monitor.Exit(_tasks);
+            }
+        }
+    }
+}
